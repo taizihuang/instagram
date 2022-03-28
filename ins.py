b6faaa17728c6d08f192fc727597a2525b8aeb6f
Taizi Huang <htzllt@gmail.com>
2022-03-28 15:21:40 +0800

    Update ins.py

--- ins.py
+++ ins.py
@@ -75,6 +75,7 @@ headers={
     'Host': 'www.instagram.com',
     'Origin': 'https://www.instagram.com',
     'upgrade-insecure-requests': '1',
+    'cookie': 'csrftoken=r0T06XDyo8HvgMtIYuIsHtbk3v1Ap5zu; mid=YkFgtQALAAEPXb6YzqZp6QDxS2ts; ig_did=999BEE1A-D887-4DBB-BC33-99A732F29270; ig_nrcb=1',
     'user-agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 10_3_1 like Mac OS X) AppleWebKit/603.1.30 (KHTML, like Gecko) Version/10.0 Mobile/14E304 Safari/602.1'
 }
 docu = requests.get(url,headers=headers).content
